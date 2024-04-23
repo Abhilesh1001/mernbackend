f995541be87d76998d29cbba05d6526b3923e45e
Abhilesh1001 <104245983+Abhilesh1001@users.noreply.github.com>
2024-04-23 12:17:25 +0530

    utll code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
+import { app } from './app.js';
 
 import connectDB from './db/index.js';
 
@@ -8,8 +9,14 @@ dotenv.config({
 })
 
 connectDB()
-
-
+.then(()=>{
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log('MONGO db connection failed !!!',err)
+})
 
 
 
