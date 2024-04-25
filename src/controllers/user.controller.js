25d248eb4c42eefa44f68feb9d005828eb262a7c
Abhilesh1001 <104245983+Abhilesh1001@users.noreply.github.com>
2024-04-25 16:06:26 +0530

    addallroutes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,8 +4,7 @@ import {User} from '../models/user.model.js'
 import {uploadOnClodinry} from '../utils/clodnary.js' 
 import {ApiResponse} from '../utils/ApiResponse.js'
 import jwt from 'jsonwebtoken'
-
-
+import mongoose from 'mongoose'
 
 
 const generateAccessAndRefreshToken = async (userId)=>{
@@ -397,4 +396,67 @@ const getUserChannelProfile = asyncHandler(async (req,res)=>{
 })
 
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,gerCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}
+const getWatchHistory = asyncHandler(async (req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match :{
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup :{
+                from :"videos",
+                localField:"watchHistory",
+                foreignField : "_id",
+                as : "watchHistory",
+                pipline:[
+                    {
+                        $lookup : {
+                            from :"users",
+                            localField : "owner",
+                            foreignField : "_id",
+                            as : "owner",
+                            pipline:[
+                                {
+                                    $project :{
+                                        fullName :1,
+                                        usernaem:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields :{
+                            owner :{
+                                $first : "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user[0].watchHistory,"Watch history fetch successfully")
+    )
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,gerCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
